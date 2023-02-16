159bd2807976a7d20e07e0d681aa1b4bb3e94c2d
Kartikey <kartikeysinha40@gmail.com>
2023-02-17 01:07:29 +0530

    insertion-sort

--- insertion_merge_sort.py
+++ insertion_merge_sort.py
@@ -1,5 +1,16 @@
-def insertion_sort():
-    pass
+def insertion_sort(unsorted_array):
+    n = len(unsorted_array)
+    
+    for i in range(1,n):
+        key = unsorted_array[i]
+        j = i - 1
+        while j > -1 and key < unsorted_array[j]:
+            # if k
+                unsorted_array[j+1] = unsorted_array[j]
+                j = j - 1 
+        unsorted_array[j+1] = key
+
+    return unsorted_array 
 
 
 
@@ -8,4 +19,8 @@ def merge_sort():
 
 
 
-# TO DO ....
+
+
+unsorted_array = [2,4,1,0,44,23,11]
+
+print(insertion_sort(unsorted_array))
