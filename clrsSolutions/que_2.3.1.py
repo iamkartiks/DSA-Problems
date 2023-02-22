316d81dbc1db33eb3067dcba869a2f1a30d32a8d
Kartikey Sinha <kartikeysinha@Kartikeys-MacBook-Air.local>
2023-02-23 01:22:01 +0530

    que-2.3.3

---
+++ clrsSolutions/que_2.3.1.py
@@ -0,0 +1,10 @@
+def merge_sort(arr):
+    pass
+
+
+
+
+if __name__ == "__main__":
+    
+    unsorted_arr = [3,41,52,26,38,57,9,49]
+    print("Sorted Array : {}".format(merge_sort(unsorted_arr)))
