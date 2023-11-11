72c4f430399d61634b524d81e068b2c043f321b1
iamkartiks <kartikeysinha40@gmail.com>
2023-11-11 23:39:54 +0530

    rust-practice

--- dummy.rs
+++ dummy.rs
@@ -1,3 +1,16 @@
-fn main(){
-    println!("Hello !");
+use std::io;
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let num: i32 =  input.trim()
+                    .parse()
+                    .expect("Please enter a valid integer");
+    println!("{}", num);
+
+    dummy(num);
+}
+
+
+fn dummy(x:i32){
+    println!("{}",x^2);
 }
--- rust-test/src/main.rs
+++ rust-test/src/main.rs
@@ -1,18 +1,12 @@
-use std::io;
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    let num: i32 =  input.trim()
-                    .parse()
-                    .expect("Please enter a valid integer");
-    println!("{}", num);
+    let mut s = String::from("hello");
 
-    dummy(&num);
+    change(&mut s);
 }
 
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 
-fn dummy(x:&i32){
-    println!("{}",x^2);
+    println!("{}", some_string);
 }
 
-// toilet age suggest crack bid produce flight observe capable nuclear speed elevator
